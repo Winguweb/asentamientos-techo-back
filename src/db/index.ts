d64fef58ff6e666a7526316f8a7d5168855efc8c
juandans01 <juandans01@gmail.com>
2021-05-18 14:57:50 -0300

    run migrations for staging

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,4 +7,8 @@ const environment = process.env.ENVIRONMENT || 'development'
 const config = require('../../knexfile.ts')[environment];
 const knex = require('knex')(config);
 
+if (process.env.NODE_ENV === 'staging') {
+  knex.migrate.latest([config]);
+}
+
 export default knex
