703255f5d69d3af039e6f8aea26fbf236b1a0537
juandans01 <juandans01@gmail.com>
2021-05-18 10:59:34 -0300

    integrate db

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+import knex from './db'
+
 const express = require( "express" );
 const app = express();
 const port = 8080; // default port to listen
-
 // define a route handler for the default home page
 app.get( "/", ( req : any , res : any ) => {
     res.send( "Hello world" );
 } );
+
+app.get('/test', async (req: any, res: any) => {
+    const result = await knex
+        .select('first_name')
+        .from('users')
+    res.json({
+        users: result
+    });
+});
 
 // start the Express server
 app.listen( port, () => {
