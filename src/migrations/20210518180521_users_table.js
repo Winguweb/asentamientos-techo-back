9cb1bbce80b7f8179a7aa2d24e516317bfbae3eb
juandans01 <juandans01@gmail.com>
2021-05-18 16:39:53 -0300

    test new migration type

--- src/migrations/20210518180521_users_table.js
+++ src/migrations/20210518180521_users_table.js
@@ -1,18 +1,14 @@
-import { Knex } from "knex";
-
-
-export async function up(knex: Knex): Promise<void> {
+exports.up = function(knex) {
   return knex.schema
-    .createTable('users', function (table : any) {
+    .createTable('users', function (table) {
        table.increments('id');
        table.string('first_name', 255).notNullable();
        table.string('last_name', 255).notNullable();
     })
-}
+};
 
-
-export async function down(knex: Knex): Promise<void> {
+exports.down = function(knex) {
   return knex.schema
       .dropTable("users");
-}
+};
 
