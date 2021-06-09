96c0ce1ceab48478229edda03f328b647634e7ff
juandans01 <juandans01@gmail.com>
2021-06-09 10:50:06 -0300

    fix merge problems

--- src/routes/settlements.router.ts
+++ src/routes/settlements.router.ts
@@ -1,17 +1,8 @@
 import express from 'express';
-<<<<<<< HEAD
 import { store, index } from '../controllers/settlements.controller';
-=======
-import { store, read, index } from '../controllers/settlements.controller';
->>>>>>> 2063b9c88daeacde2320fd492271680a3dd238e9
 
 
 export const settlementsRouter = express.Router();
 
 settlementsRouter.post('/', store);
-<<<<<<< HEAD
 settlementsRouter.get('/', index);
-=======
-settlementsRouter.get('/', read);
-settlementsRouter.get('/index', index);
->>>>>>> 2063b9c88daeacde2320fd492271680a3dd238e9
