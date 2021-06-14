4792200ba642af43547d97a4758299f7b6358733
juandans01 <juandans01@gmail.com>
2021-06-14 17:23:29 -0300

    add covid search

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -9,7 +9,30 @@ export const search = async (req: Request, res: Response) => {
   const result: Array<ISettlement> = 
     await knex('settlements')
     .where('name', 'like', `%${q}%`)
-    .select('name', 'country', 'province', 'municipality', 'zone', 'latitud', 'longitude', 'status', 'population')
+    .select('id', 'name', 'country', 'province', 'municipality', 'zone', 'latitud', 'longitude', 'status', 'population')
+
+  res.json(result);
+}
+
+export const searchCovid = async (req: Request, res: Response) => {
+  const q:string = req.query.q as string;
+
+  const result: Array<ISettlement> = 
+    await knex('settlements')
+    .where('settlements.name', 'like', `%${q}%`)
+    .distinctOn('settlements.name')
+    .join('covid', 'covid.settlement_id', '=', 'settlements.settlement_id')
+    .select('settlements.name', 
+      'settlements.settlement_id', 
+      'settlements.country', 
+      'settlements.province', 
+      'settlements.municipality', 
+      'settlements.zone', 
+      'settlements.latitud', 
+      'settlements.longitude', 
+      'settlements.status', 
+      'settlements.population'
+    )
 
   res.json(result);
 }
--- src/routes/search.router.ts
+++ src/routes/search.router.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
 
-import { search } from '../controllers/search.controller'
+import { search, searchCovid } from '../controllers/search.controller'
 
 
 export const searchRouter = express.Router();
 
 searchRouter.get('/', search);
+searchRouter.get('/covid', searchCovid);
