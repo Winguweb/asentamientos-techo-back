f7d00d00c397e5bfd3f07d8f40f8fbf31de2fc0f
juandans01 <juandans01@gmail.com>
2021-06-15 13:16:20 -0300

    test routes

--- src/routes/search.router.ts
+++ src/routes/search.router.ts
@@ -5,5 +5,5 @@ import { search, searchCovid } from '../controllers/search.controller'
 
 export const searchRouter = express.Router();
 
-searchRouter.get('/', search);
-searchRouter.get('/covid', searchCovid);
+searchRouter.get('/co', searchCovid);
+searchRouter.get('/ge', search);
