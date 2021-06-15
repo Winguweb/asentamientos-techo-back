f43975533c26756996848ffa3546b5a2a56d5be0
juandans01 <juandans01@gmail.com>
2021-06-15 12:56:08 -0300

    fix covid query

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -15,23 +15,25 @@ export const search = async (req: Request, res: Response) => {
 }
 
 export const searchCovid = async (req: Request, res: Response) => {
+  console.log('on search covid')
   const q:string = req.query.q as string;
 
   const result: Array<ISettlement> = 
-    await knex('settlements')
-    .where('settlements.name', 'like', `%${q}%`)
-    .distinctOn('settlements.name')
-    .join('covid', 'covid.settlement_id', '=', 'settlements.settlement_id')
-    .select('settlements.name', 
-      'settlements.settlement_id', 
-      'settlements.country', 
-      'settlements.province', 
-      'settlements.municipality', 
-      'settlements.zone', 
-      'settlements.latitud', 
-      'settlements.longitude', 
-      'settlements.status', 
-      'settlements.population'
+    await knex('covid')
+    .where('covid.name', 'like', `%${q}%`)
+    .distinctOn('covid.name')
+    // .join('covid', 'covid.settlement_id', '=', 'settlements.settlement_id')
+    .select(
+      'covid.name', 
+      'covid.latitude', 
+      'covid.longitude',
+      'covid.settlement_id' 
+      // 'settlements.municipality', 
+      // 'settlements.zone', 
+      // 'settlements.latitud', 
+      // 'settlements.longitude', 
+      // 'settlements.status', 
+      // 'settlements.population'
     )
 
   res.json(result);
