5022c17e4e2c836ed8919ae903be27d768fd9069
Franco Alemandi <francolp59@gmail.com>
2021-06-25 09:43:20 -0300

    add migrants to settlements

---
+++ src/migrations/20210625123959_add_migrants_to_settlements.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+    return knex.schema.table('settlement_features', table => {
+        table.string('returned_migrants', 255);
+        table.string('country_people_returned', 255);
+    })
+  }
+  
+  exports.down = function(knex) {
+    return knex.schema.table('covid', table => {
+        table.string('returned_migrants', 255);
+        table.string('country_people_returned', 255);
+    })
+  };
+      
